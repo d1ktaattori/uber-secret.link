4d331e21e4bdefcfaa7d551db5965e199519aded
Kaiser <67063927+d1ktaattori@users.noreply.github.com>
2026-02-25 01:42:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ function checkAnswer() {
 		document.getElementById("error").innerText = "blud thought that was the word";
 	}
 }
+
+window.onload = function () {
+	positionRandomly("realBtn");
+	positionRandomly("fakeBtn");
+};
+
+function positionRandomly(id) {
+	const button = document.getElementById(id);
+
+const padding = 100;
+
+const maxX = window.innerWidth - padding;
+const maxY = window.innerHeight - padding ;
+
+const randomX = Math.random() * maxX;
+const randomY = Math.random() * maxY;
+
+button.style.left = randomX + "px";
+button.style.top = randomY + "px";
+}
